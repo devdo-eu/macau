97e41edbd342f3db980daaa5ec09504bb8ff5173
Grzegorz Maciaszek <grzesiek@devdo.eu>
2021-03-10 19:05:11 +0100

    Work in progress on CPU player. Mundane play plan and play with ace plan implemented/tested.

--- player/player.py
+++ player/player.py
@@ -1,4 +1,6 @@
 import os
+from secrets import choice
+from copy import copy
 
 
 class Player:
@@ -54,3 +56,50 @@ class Player:
                f'\n*color value* -> means that this card can be played' \
                f'\nWhich card(s) from your hand do you want to play?: '
         return gui
+
+
+class CPUPlayer(Player):
+    def __init__(self, name):
+        Player.__init__(self, name)
+        self.next_moves = []
+        self.move_counter = -1
+        self.gui_foo = self.__cpu_gui
+        self.print_foo = self.__cpu_print
+
+    def __cpu_gui(self, _game_state, _top_card, possible_plays):
+        self.next_moves = [choice(possible_plays)]
+        if self.next_moves[0][1] == 'A':
+            biggest_color, _ = self.find_biggest('color')
+            self.next_moves.append(biggest_color)
+        if self.next_moves[0][1] == 'J':
+            self.next_moves.append(self.evaluate_jack_request())
+
+    def evaluate_jack_request(self):
+        biggest_value, appearances_value = self.find_biggest('value')
+        biggest_color, appearances_color = self.find_biggest('color')
+        if appearances_color > appearances_value:
+            biggest_cards = [card for card in self.hand if card[0] == biggest_color]
+        else:
+            biggest_cards = [card for card in self.hand if card[1] == biggest_value]
+        chosen_card = choice(biggest_cards)
+        return chosen_card[1]
+
+    def find_biggest(self, what='color'):
+        index = bool(what != 'color')
+        hand_copy = copy(self.hand)
+        hand_copy.remove(self.next_moves[0])
+        on_hand = {}
+        for card in hand_copy:
+            trait = card[index]
+            if trait in on_hand.keys():
+                on_hand[trait] += 1
+            else:
+                on_hand[trait] = 1
+        biggest = max(on_hand, key=on_hand.get)
+        return biggest, on_hand[biggest]
+
+    def __cpu_print(self, _message):
+        pass
+
+    #def cpu_input(self, _message):
+    #    self.move_counter += 1
--- player/test_player.py
+++ player/test_player.py
@@ -1,7 +1,17 @@
 import game
+import pytest
+from player.player import CPUPlayer
 
 
-def test_game_state():
+@pytest.fixture
+def gs_cpu():
+    gs = game.GameState()
+    cpu = CPUPlayer('cpu')
+    gs.players['cpu'] = cpu
+    return gs
+
+
+def test_default_player_gui():
     gs = game.GameState()
     gs.deck, gs.table, gs.players = game.prepare_game(['1', '2'])
     possible_plays = []
@@ -14,3 +24,37 @@ def test_game_state():
     assert len(messages[0]) > 0
     assert len(messages[1]) > 0
     assert messages[0] != messages[1]
+
+
+def test_cpu_player_init():
+    cpu = CPUPlayer('cpu')
+    assert cpu.next_moves == []
+    assert cpu.move_counter == -1
+    assert cpu.name == 'cpu'
+
+
+def test_cpu_mundane_move(gs_cpu):
+    cpu = gs_cpu.players['cpu']
+    possible_plays = [('hearts', '6')]
+    top_card = ('hearts', '9')
+    cpu.gui_foo(gs_cpu, top_card, possible_plays)
+    assert len(cpu.next_moves) == 1
+    assert ('hearts', '6') in cpu.next_moves
+
+
+def test_cpu_ace_move(gs_cpu):
+    cpu = gs_cpu.players['cpu']
+    possible_plays = [('pikes', 'A')]
+    cpu.hand = [('pikes', 'A'), ('pikes', '6'), ('tiles', '8'), ('tiles', '6')]
+    cpu.gui_foo(gs_cpu, ('pikes', '5'), possible_plays)
+    assert len(cpu.next_moves) == 2
+    assert 'tiles' in cpu.next_moves
+    assert ('pikes', 'A') in cpu.next_moves
+    assert len(cpu.hand) == 4
+
+    cpu.hand = [('pikes', 'A'), ('pikes', '6'), ('tiles', '8'), ('tiles', '6'), ('pikes', '7')]
+    cpu.gui_foo(gs_cpu, ('pikes', '5'), possible_plays)
+    assert len(cpu.next_moves) == 2
+    assert 'pikes' in cpu.next_moves
+    assert ('pikes', 'A') in cpu.next_moves
+    assert len(cpu.hand) == 5
