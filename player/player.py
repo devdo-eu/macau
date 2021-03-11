defe706f928908457090f360085e2f14a44bd073
Grzegorz Maciaszek <grzesiek@devdo.eu>
2021-03-11 09:09:45 +0100

    Improved cpu player play. Some bugs hammered.

--- player/player.py
+++ player/player.py
@@ -70,23 +70,63 @@ class CPUPlayer(Player):
         self.print_foo = self.__cpu_print
         self.input_foo = self.__cpu_input
 
-    def __cpu_gui(self, _game_state, _top_card, possible_plays):
+    def __cpu_gui(self, game_state, _top_card, possible_plays):
         """
         Function used to get gui information to cpu player.
         It is responsible for building strategy of this player.
-        :param _game_state: GameState object with all information about state of game
+        :param game_state: GameState object with all information about state of game
         :param _top_card: tuple with card on top of a table
         :param possible_plays: list of cards possible to be played
         :return: empty string object
         """
         self.move_counter = -1
-        self.next_moves = [choice(possible_plays)]
-        if self.next_moves[0][1] == 'A':
+        self.__choose_first_move(game_state, possible_plays)
+        if len(self.next_moves[0]) > 1 and self.next_moves[0][1] == 'A':
             biggest_color, _ = self.__find_biggest('color')
             self.next_moves.append(biggest_color)
-        if self.next_moves[0][1] == 'J':
+        if len(self.next_moves[0]) > 1 and self.next_moves[0][1] == 'J':
             self.next_moves.append(self.__evaluate_jack_request())
         return ''
+
+    def __choose_first_move(self, game_state, possible_plays):
+        """
+        Helper function used to choose first move of cpu player.
+        :param game_state: GameState object with all information about state of game
+        :param possible_plays: list of cards possible to be played
+        """
+        if len(possible_plays) == 0:
+            self.next_moves = ['']
+        else:
+            if not self.__need_to_attack(game_state):
+                self.next_moves = [choice(possible_plays)]
+            else:
+                offensive_plays = []
+                for card in possible_plays:
+                    if card == ('pikes', 'K') or card == ('hearts', 'K'):
+                        offensive_plays.append(card)
+                    elif card[1] in '2 3 4 J'.split():
+                        offensive_plays.append(card)
+                if len(offensive_plays) > 0:
+                    self.next_moves = [choice(offensive_plays)]
+                else:
+                    self.next_moves = [choice(possible_plays)]
+
+    def __need_to_attack(self, game_state):
+        """
+        Helper function used to evaluate if attacking now is good strategy.
+        :param game_state: GameState object with all information about state of game
+        :return: bool True if cpu should attack, False otherwise
+        """
+        copy_players = list(game_state.players.values()) + list(game_state.players.values())
+        after_me = 0
+        need_to_attack = False
+        for rival in copy_players:
+            if rival.name == self.name:
+                after_me = 2
+            if after_me > 0 and len(rival.hand) < 3:
+                need_to_attack = True
+            after_me -= 1
+        return need_to_attack
 
     def __evaluate_jack_request(self):
         """
@@ -102,7 +142,9 @@ class CPUPlayer(Player):
                              if card[0] == biggest_color and card[1] not in '2 3 4 J Q K A'.split()]
         else:
             biggest_cards = [card for card in hand_copy if card[1] == biggest_value]
-        chosen_card = choice(biggest_cards)
+        chosen_card = ('', '')
+        if len(biggest_cards) > 0:
+            chosen_card = choice(biggest_cards)
         return chosen_card[1]
 
     def __find_biggest(self, what='color'):
@@ -124,8 +166,11 @@ class CPUPlayer(Player):
                 on_hand[trait] = 1
         if index == 1:
             [on_hand.pop(value) for value in '2 3 4 J Q K A'.split() if value in on_hand.keys()]
-        biggest = max(on_hand, key=on_hand.get)
-        return biggest, on_hand[biggest]
+        biggest = max(on_hand, key=on_hand.get, default='')
+        appearances = 0
+        if biggest in on_hand:
+            appearances = on_hand[biggest]
+        return biggest, appearances
 
     def __cpu_print(self, _message):
         """
@@ -141,9 +186,11 @@ class CPUPlayer(Player):
         :return: string with next move of cpu player
         """
         self.move_counter += 1
+        cpu_move = ''
         if len(self.next_moves) > self.move_counter:
             if type(self.next_moves[self.move_counter]) is str:
-                return self.next_moves[self.move_counter]
+                cpu_move = self.next_moves[self.move_counter]
             else:
-                return f'{self.next_moves[self.move_counter][0]} {self.next_moves[self.move_counter][1]}'
-        return ''
+                cpu_move = f'{self.next_moves[self.move_counter][0]} {self.next_moves[self.move_counter][1]}'
+        print(f'{self.name} plays: {cpu_move}, on hand left: {len(self.hand) - 1} cards')
+        return cpu_move
