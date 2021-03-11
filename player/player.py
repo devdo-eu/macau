3d2342d50c7a2462f68d897e2ddfb7a1871587f2
Grzegorz Maciaszek <grzesiek@devdo.eu>
2021-03-11 16:28:37 +0100

    Next functions and methods covered with unit tests.

--- player/player.py
+++ player/player.py
@@ -79,6 +79,22 @@ def find_offensive_plays(possible_plays):
     return offensive_plays
 
 
+def find_best_attack_card(possible_plays):
+    """
+    Helper function used to find best card to play as attack.
+    :param possible_plays: list of cards possible to be played
+    :return: list with one element with the best card to be played
+    """
+    offensive_plays = find_offensive_plays(possible_plays)
+    if len(offensive_plays) > 0:
+        return [choice(offensive_plays)]
+    else:
+        plays_no_queens = [card for card in possible_plays if card[1] != 'Q']
+        if len(plays_no_queens) > 0:
+            return [choice(plays_no_queens)]
+    return ['']
+
+
 class CPUPlayer(Player):
     def __init__(self, name):
         Player.__init__(self, name)
@@ -118,11 +134,7 @@ class CPUPlayer(Player):
             if not self.need_to_attack(game_state):
                 self.next_moves = [choice(possible_plays)]
             else:
-                offensive_plays = find_offensive_plays(possible_plays)
-                if len(offensive_plays) > 0:
-                    self.next_moves = [choice(offensive_plays)]
-                else:
-                    self.next_moves = [choice(possible_plays)]
+                self.next_moves = find_best_attack_card(possible_plays)
 
     def need_to_attack(self, game_state):
         """
