f92f233f6183c4415bcca210e49b9eda4deff4d2
Grzegorz Maciaszek <grzesiek@devdo.eu>
2021-03-24 17:50:02 +0100

    Improved unit tests coverage.

--- macau_gui_client.py
+++ macau_gui_client.py
@@ -1,4 +1,23 @@
-import gui_rest_client.macau_gui as gui
+import pyglet
+import asyncio
+import gui_rest_client.common as common
+from gui_rest_client.macau_gui import MacauGUI
+from gui_rest_client.menu_window import MenuWindow
+from gui_rest_client.game_window import GameWindow
+
+
+def prepare_gui(display_factory=pyglet.canvas.Display, window_factory=pyglet.window.Window):
+    pyglet.resource.path = [common.build_resources_path()]
+    pyglet.resource.reindex()
+    card_images = common.load_all_card_images()
+    display = display_factory()
+    screen = display.get_default_screen()
+    menu_window = MenuWindow(screen, card_images, window_factory)
+    game_window = GameWindow(screen, card_images, window_factory)
+    macau_gui = MacauGUI(menu_window, game_window)
+    return macau_gui
+
 
 if __name__ == "__main__":
-    gui.main()
+    gui = prepare_gui()
+    gui.loop.run_until_complete(asyncio.gather(*gui.tasks))
